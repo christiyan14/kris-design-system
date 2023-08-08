28bda6f4358ee65ec77c064848b369aed774234e
Christiyan <christiyanweb@gmail.com>
2023-08-08 22:59:11 +0300

    [feat] Init project with figma tokens and style-dictionary

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.scss";
+import Button from "./components/button";
+
+function App() {
+  return <Button label="Click me" />;
+}
+
+export default App;
---
+++ src/components/button.tsx
@@ -0,0 +1,21 @@
+import { FC, MouseEvent } from "react";
+import "../assets/style/button.scss";
+
+interface ButtonProps {
+  label: string;
+  type?: "primary" | "secondary";
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button: FC<ButtonProps> = ({ label, type = "primary", onClick }) => {
+  const buttonClass =
+    type === "primary" ? "button-primary" : "button-secondary";
+
+  return (
+    <button className={buttonClass} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
