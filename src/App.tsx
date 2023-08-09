fb7f0ab8d99a42cdcb22cd64ee58510eafc64c0f
kristiyan.velkov <kristiyan.velkov@ffw.com>
2023-08-09 15:44:48 +0300

    [feat] Update buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import "./App.scss";
 import Button from "./components/button/Button";
 
 function App() {
-  return <Button label="Button" />;
+  return (
+    <>
+      <section className="button-section">
+        <Button label="Button" />
+
+        <Button label="Button" type="secondary" />
+      </section>
+    </>
+  );
 }
 
 export default App;
